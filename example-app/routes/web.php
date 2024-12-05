2bce41a7e91d7020c15ad98c588de2543172eac1
j-zabalabur <146177739+j-zabalabur@users.noreply.github.com>
2024-12-05 10:27:54 +0000

    Prueba routes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/note', [NoteController::class, 'index']);
